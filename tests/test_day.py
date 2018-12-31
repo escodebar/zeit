fe35f1bcd0563b0919735bf3be5833c016e4713f
Pablo Escodebar <escodebar@gmail.com>
2018-12-31 15:52:29 +0100

    Test that saturdays do not generate minus hours
    We are not forced to work on saturdays, therefore they shouldn't
    contribute to minus hours.

---
+++ tests/test_day.py
@@ -0,0 +1,22 @@
+from zeit.reader import file_reader
+import datetime
+import pytest
+
+
+@pytest.fixture
+def december(tmp_path):
+    _file = tmp_path / "2018-12"
+    _file.write_text("\n")
+    yield file_reader(_file)
+
+
+@pytest.fixture
+def saturday(december):
+    saturday = next(december)
+    assert 6 == saturday.date.isoweekday()
+    yield saturday
+
+
+@pytest.mark.xfail
+def test_saturday_does_not_count_against_minus_hours(saturday):
+    assert datetime.timedelta() == saturday.minus_hours
