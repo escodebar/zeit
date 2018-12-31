61a0b5bd3f49e002c679ed0ea3212e20a87b2afe
Pablo Escodebar <escodebar@gmail.com>
2018-12-31 14:36:14 +0100

    Test date of day objects
    Each line of a file corresponds to a day of the month. The first line
    corresponds to the first day, the second line to the second day etc. The
    day objects should have a date attribute which is generated using the
    name of the file and the line number.

--- tests/test_read_timesheet.py
+++ tests/test_read_timesheet.py
@@ -1,12 +1,13 @@
 from zeit.models import Day
 from zeit.reader import file_reader
+import datetime
 import pytest
 
 
 @pytest.fixture
 def data_file(tmp_path):
     _file = tmp_path / "2018-12"
-    _file.write_text("\n")
+    _file.write_text("\n\n")
     yield _file
 
 
@@ -14,3 +15,10 @@ def test_file_reader_generates_day_objects(data_file):
     test_data_reader = file_reader(data_file)
     day = next(test_data_reader)
     assert isinstance(day, Day)
+
+
+@pytest.mark.xfail
+def test_day_of_the_month_is_given_by_line_number(data_file):
+    test_data_reader = file_reader(data_file)
+    assert datetime.date(2018, 12, 1) == next(test_data_reader).date
+    assert datetime.date(2018, 12, 2) == next(test_data_reader).date
