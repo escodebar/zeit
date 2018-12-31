5b3e293902adfa0045b797d970eb238370a09132
Pablo Escodebar <escodebar@gmail.com>
2018-12-31 14:03:03 +0100

    Move test data to a fixture
    Keeps the test data closer to the tests.

--- tests/test_read_timesheet.py
+++ tests/test_read_timesheet.py
@@ -1,8 +1,16 @@
 from zeit.models import Day
 from zeit.reader import file_reader
+import pytest
 
 
-def test_file_reader_generates_day_objects():
-    test_data_reader = file_reader('tests/data/2018-12')
+@pytest.fixture
+def data_file(tmp_path):
+    _file = tmp_path / "2018-12"
+    _file.write_text("\n")
+    yield _file
+
+
+def test_file_reader_generates_day_objects(data_file):
+    test_data_reader = file_reader(data_file)
     day = next(test_data_reader)
     assert isinstance(day, Day)
