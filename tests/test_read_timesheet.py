11512078f2cb86feb1f75f9b4abe58e380b92513
Pablo Escodebar <escodebar@gmail.com>
2018-12-31 14:52:49 +0100

    Rename data file to december file
    To make the name of file more explicit.

--- tests/test_read_timesheet.py
+++ tests/test_read_timesheet.py
@@ -5,20 +5,20 @@ import pytest
 
 
 @pytest.fixture
-def data_file(tmp_path):
+def december_file(tmp_path):
     _file = tmp_path / "2018-12"
     _file.write_text("\n\n")
     yield _file
 
 
-def test_file_reader_generates_day_objects(data_file):
-    test_data_reader = file_reader(data_file)
+def test_file_reader_generates_day_objects(december_file):
+    test_data_reader = file_reader(december_file)
     day = next(test_data_reader)
     assert isinstance(day, Day)
 
 
 @pytest.mark.xfail
-def test_day_of_the_month_is_given_by_line_number(data_file):
-    test_data_reader = file_reader(data_file)
+def test_day_of_the_month_is_given_by_line_number(december_file):
+    test_data_reader = file_reader(december_file)
     assert datetime.date(2018, 12, 1) == next(test_data_reader).date
     assert datetime.date(2018, 12, 2) == next(test_data_reader).date
